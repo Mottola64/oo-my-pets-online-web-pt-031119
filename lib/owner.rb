6cfe89fec6188a684db66fa690f8929f99ea71c0
Michael Mottola <mottola64@gmail.com>
2019-04-14 21:13:14 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,18 @@ class Owner
   attr_accessor :name, :pets
   attr_reader :species
   
-  def initialize(name)
-    @name = name
+def initialize(species)
+    @species = species
 end
+
+def self.clear
+  OWNERS.clear
+end
+
+def self.count
+  OWNERS.size
+end
+
+
+
 end
